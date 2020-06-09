096b8f69b6488b17c90aaf34f91b0dc2844a760a
User <User@192.168.3.127>
2020-06-09 14:07:37 +0800

    branch test

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -4,6 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		//test
+		//test branch 
 		Coach theCoach = new TrackCoach();
 		System.out.println(theCoach.getDailyWorkout());
 	}

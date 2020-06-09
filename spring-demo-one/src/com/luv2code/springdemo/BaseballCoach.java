71591103007da35cf762474a9ea74aca2fa48a0a
User <User@192.168.3.127>
2020-06-09 12:02:06 +0800

    little change

--- spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -4,7 +4,7 @@ public class BaseballCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
-		return "spend 30 minutes on batting practice";
+		return "spend 40 minutes on batting practice";
 	}
 	
 }

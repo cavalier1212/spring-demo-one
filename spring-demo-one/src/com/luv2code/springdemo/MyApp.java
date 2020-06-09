c68a448048593cb2ceaa0ad0f243ac7a6dad3d2a
cavalier <cavalier@192.168.1.105>
2020-06-09 23:20:45 +0800

    home test to branch

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -5,6 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		//test
 		//test branch 
+		//home test to branch
 		Coach theCoach = new TrackCoach();
 		System.out.println(theCoach.getDailyWorkout());
 	}

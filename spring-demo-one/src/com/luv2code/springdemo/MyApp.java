3b6bb7846e64f487499a26aacceeb703d3861036
User <User@192.168.3.127>
2020-06-09 12:02:39 +0800

    little change 2

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
+		//test
 		Coach theCoach = new TrackCoach();
 		System.out.println(theCoach.getDailyWorkout());
 	}
